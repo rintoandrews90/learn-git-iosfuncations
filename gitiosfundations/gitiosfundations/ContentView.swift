b0ff1380c87b709eaf1587e49fba3879941c11ef
Rinto Andrews <rintoandrews90@gmail.com>
2025-03-11 21:34:44 +0200

    first commit

---
+++ gitiosfundations/gitiosfundations/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  gitiosfundations
+//
+//  Created by Rinto Andrews on 11/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ gitiosfundations/gitiosfundations/gitiosfundationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitiosfundationsApp.swift
+//  gitiosfundations
+//
+//  Created by Rinto Andrews on 11/03/25.
+//
+
+import SwiftUI
+
+@main
+struct gitiosfundationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
